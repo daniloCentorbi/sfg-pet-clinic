28318f272cbdcaa83ac7603fbad7dca8ebf39627
dani <danilo.centorby@gmail.com>
2019-06-30 12:03:47 +0800

    refactoring Model Lombok. closes #9

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,25 +1,26 @@
 package guru.springframework.sfgpetclinic.model;
 
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
-    @Column( name = "name")
-    private String name;
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
         this.name = name;
     }
 
+    @Column( name = "name")
+    private String name;
 }
